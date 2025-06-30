34b16633992dbf6b52671b0d9b80fcf27295b2bb
Sambit Kumar pattanaik <Sambitpattanaik52@gmail.com>
2025-06-30 10:29:35 +0530

    Update hello-world.php

--- hello-world.php
+++ hello-world.php
@@ -1,4 +1,6 @@
 <?php
 echo "Hello, World! This is a simple PHP script.";
-echo "\nThis script demonstrates basic PHP syntax.";
+echo "\n This script demonstrates basic PHP syntax.<br>";
+
+echo "echo is a language construct in PHP used to output one or more strings or variables to the standard output (typically, the browser).<br> It is commonly used to display text, HTML, or variable values.";
 ?>
